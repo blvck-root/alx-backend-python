4e3a34f4070e11709a32cfb7336b602eefdee5ed
blvck-root <mtndlovu81@gmail.com>
2024-07-05 04:27:13 +0200

    fix type errors in 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,13 @@
 """Type-annotated add function."""
 
 
-def add(a: int, b: float) -> float:
-    """Returns sum of two floats."""
+def add(a: float, b: float) -> float:
+    """
+    Type-annotated add function.
+    Args:
+        a (int): The first number.
+        b (float): The second number.
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
